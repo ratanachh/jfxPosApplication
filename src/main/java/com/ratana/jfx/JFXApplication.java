00cbf587bfa70397225349d8143590b918a424c7
Chhorm Ratana <chhormratana@gmail.com>
2022-11-10 14:09:25 +0700

    update style

--- src/main/java/com/ratana/jfx/JFXApplication.java
+++ src/main/java/com/ratana/jfx/JFXApplication.java
@@ -4,11 +4,9 @@ import com.ratana.jfx.controller.LoginController;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
