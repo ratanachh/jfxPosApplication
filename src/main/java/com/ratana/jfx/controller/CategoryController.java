9ac57ef33c6b737347dd14ae8763c18fea0da305
Ratana CHHORM <chhormratana@gmail.com>
2022-11-16 08:44:01 +0700

    create view of each menu item

---
+++ src/main/java/com/ratana/jfx/controller/CategoryController.java
@@ -0,0 +1,7 @@
+package com.ratana.jfx.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CategoryController {
+}
--- src/main/java/com/ratana/jfx/controller/MainFrameController.java
+++ src/main/java/com/ratana/jfx/controller/MainFrameController.java
@@ -1,8 +1,13 @@
 package com.ratana.jfx.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Controller;
 
@@ -10,6 +15,17 @@ import java.util.Objects;
 
 @Controller
 public class MainFrameController {
+    @FXML
+    private VBox sidebar;
+    @FXML
+    private StackPane contentView;
+
+    @FXML
+    private void clickMenu(MouseEvent event) {
+        Node node = (Node) event.getSource();
+        System.out.println(node.getId() );
+    }
+
     public static void show() {
         try {
             Stage stage = new Stage();
